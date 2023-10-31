abc6210e11578565c1487f4c251b4773352a5406
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-10-31 16:36:41 -0500

    Add bgm types. Extract pmd id type.

---
+++ Libellus Library/Event/Types/Frame/PmdBgmType.cs
@@ -0,0 +1,13 @@
+﻿namespace LibellusLibrary.Event.Types.Frame;
+
+public enum PmdBgmType
+    : ushort
+{
+    BGM_PLAY,
+    BGM_FADE_IN,
+    BGM_FADE_OUT,
+    BGM_TRANSITION,
+    BGM_VOLUME_DOWN,
+    BGM_VOLUME_UP,
+    BGM_ALL_STOP,
+}
